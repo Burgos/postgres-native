ff33c7ae56ef38dfb7fd44c3ed34c4a0bb8053ab
Nemanja Boric <4burgos@gmail.com>
2016-12-22 10:27:38 +0100

    Make number of iterations configurable

--- src/app.d
+++ src/app.d
@@ -1,18 +1,28 @@
 module app;
 
-void main()
+void main(string[] args)
 {
     import message;
     import connection;
     import std.stdio;
+    import std.conv;
 
     // try to connect to the
     // postgres, and see what we have
     auto conn = Connection("127.0.0.1", 5432, "burgos", "test-pass",
             "test");
     conn.connect();
-    writeln("\tExecuting extended query: ");
-    conn.query("SELECT * FROM stripovi WHERE id >= $1 and junak = $2", 1, "Zagor");
-    writeln("\tExecuting normal query: ");
-    conn.query("SELECT * FROM stripovi WHERE id > 1");
+
+    auto num_iterations = 1;
+    if (args.length > 1)
+    {
+        num_iterations = to!(int)(args[1]);
+    }
+
+
+    for(int i = 0; i < num_iterations; i++)
+    {
+        conn.query("SELECT * FROM stripovi WHERE id >= $1 and junak = $2", 1, "Zagor");
+        conn.query("SELECT * FROM stripovi WHERE id > 1");
+    }
 }
